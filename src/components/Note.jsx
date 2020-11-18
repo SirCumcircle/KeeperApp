97fe8f32368341d1415941437af69b5dc290508d
SirCumcircle <zakiattaulla@gmail.com>
2020-11-18 16:55:48 +0530

    Add key to react components

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,8 +4,8 @@ import notes from "../notes"
 
 
 let newNote = (i) => {
-  return(<div className="note">
-    <h1 key={i.key}>{i.title}</h1>
+  return(<div className="note" key={i.key}>
+    <h1>{i.title}</h1>
     <p>{i.content}</p>
   </div>)
 }
